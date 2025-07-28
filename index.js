99e7dec4153312ddb3ffd1b0f55cde1e121c94f2
wizzhy <ujangkasmin4@gmail.com>
2025-07-28 14:59:31 +0700

    Latihan JavaScript

---
+++ index.js
@@ -0,0 +1,61 @@
+// function sapa(nama) {
+//     console.log("Halo, " + nama + "!");
+// }
+
+// sapa("Joy");
+// sapa("Seulgi");
+
+// function tambah(a, b) {
+//     return a + b;
+// }
+
+// let hasil = tambah(5, 3);
+// console.log("Hasil: " + hasil);
+
+// let car = {
+//     name: "Flat",
+//     model: 500,
+//     weight: "30kg",
+//     color: "yellow",
+
+//     start: function() {
+//         console.log("Mobil dinyalakan.");
+//     },
+//     drive: function() {
+//         console.log("Mobil berjalan");
+//     },
+//     brake: function() {
+//         console.log("Mobil mengerem");
+//     },
+//     stop: function() {
+//         console.log("Mobil berhenti");
+//     },
+// };
+
+// console.log(car.name);
+// console.log(car.color);
+
+// car.start();
+// car.drive();
+
+let bike = {
+    merk: "polygon",
+    jumgear: 50,
+    color: "yellow",
+
+    start: function() {
+        console.log("sepeda mulai dikayuh.")
+    },
+    stop: function() {
+        console.log("sepeda mengerem.")
+    },
+    
+};
+
+console.log(bike.merk);
+console.log(bike.jumgear);
+console.log(bike.color);
+
+
+bike.start();
+bike.stop();
